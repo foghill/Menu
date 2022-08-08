ceff15c3a1a4f9e668c6af1a63505dd28beb4456
Benjamin <ben@thefoghill.com>
2022-08-08 18:36:57 -0400

    Categories re-mapping

--- src/Categories.js
+++ src/Categories.js
@@ -3,16 +3,12 @@ import React from "react";
 const Categories = ({ categories, filterItems }) => {
   return (
   <div className="btn-container">
-    <button className="filter-btn" onClick={() => filterItems("all")}>
-      All
+   {categories.map((category,index)=>
+   {
+    return <button type='button' className='filter-btn' key={index} onClick ={() => filterItems(category)}>
+      {category}
     </button>
-    <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-      breakfast
-    </button>
-    <button className="filter-btn" onClick={() => filterItems("lunch")}>
-      lunch
-    </button>
-
+   })}
   </div>
   )
 };
